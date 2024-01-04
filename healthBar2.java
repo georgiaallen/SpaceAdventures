93985993da93e7b825faad42190daeabb7f7cd69
georgiaallen <147187454+georgiaallen@users.noreply.github.com>
2024-01-04 10:49:07 +0000

    Comments healthBar2.java

--- healthBar2.java
+++ healthBar2.java
@@ -1,41 +1,49 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class healthBar2 here.
+ * Represents a health bar for UFO & UFO2 in Mission 1 & 2.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Author: (Georgia)
+ * 
  */
 public class healthBar2 extends Actor
 {
-    /**
-     * Act - do whatever the healthBar2 wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    private int health = 5;
-    int healthBarWidth = 100;
-    int healthBarHeight = 17;
-   int pixelsPerHealth = (int)healthBarWidth/health;
+    // Instance variables
+    private int health = 5;  // Set health of UFO & UFO2
+    int healthBarWidth = 100;  // Width of the health bar
+    int healthBarHeight = 17;  // Height of the health bar
+    int pixelsPerHealth = (int)healthBarWidth/health;  // Pixels per health point
 
+    /**
+     * Constructor for the healthBar2 class.
+     * Initializes health to 5 and calls the update method.
+     */
     public healthBar2(){
-    health = 5;
-     
-    update();
-}
+        health = 5;
+        update();
+    }
+
+    /**
+     * Act method - called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     * Updates the health bar and checks for keyboard input to move the object to create a scrolling effect
+     */
     public void act()
     {
-        update();
-         if(Greenfoot.isKeyDown("right"))
+        update();  // Refresh the appearance of the health bar
+        if(Greenfoot.isKeyDown("right"))
         {
-            move(3);
+            move(3);  // Move the object to the right
         }
         if(Greenfoot.isKeyDown("left"))
         {
-            move(-3);
+            move(-3);  // Move the object to the left
         }
     }
 
-
+    /**
+     * Updates the appearance of the health bar.
+     * Draws a white rectangle around the health bar and fills it with a blue color based on the current health.
+     */
     public void update(){
         setImage(new GreenfootImage(healthBarWidth + 2, healthBarHeight + 2));
         GreenfootImage myImage = getImage();
@@ -44,15 +52,18 @@ public class healthBar2 extends Actor
         myImage.setColor(Color.BLUE);
         myImage.fillRect(1,1, health*pixelsPerHealth,healthBarHeight);
     }
-    
-    
 
+    /**
+     * Decreases the health of the actor by one.
+     */
     public void loseHealth() {
-        health --; 
+        health--; 
     }
 
+    /**
+     * Returns the current health of the actor.
+     */
     public int getHealth() {
         return health;
     }
-    
 }
