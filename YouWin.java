9bd6bb0a30c77c07ebab755c0ee08f8414d99750
georgiaallen <147187454+georgiaallen@users.noreply.github.com>
2024-01-04 11:03:53 +0000

    Update YouWin.java

--- YouWin.java
+++ YouWin.java
@@ -1,15 +1,20 @@
-import greenfoot.*;  
+import greenfoot.*;
 
 /**
- * You win screen for astronaut and astronaut2 actors
+ * Represents a "You Win" screen for astronaut and astronaut2 actors.
  * 
- * @author (Georgia) 
  * 
+ * Author: Georgia
  */
 public class YouWin extends World
 {
+    /**
+     * Constructor for YouWin class.
+     * Sets up the "You Win" screen with a specified size.
+     * 
+     */
     public YouWin()
     {    
-        super(3000, 700, 1);//screen size
+        super(3000, 700, 1); // Set the screen size with width 3000, height 700, and 1 cell
     }
 }
