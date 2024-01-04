a4969303c5f9cc912e823815b9b2fbc5c81030e6
georgiaallen <147187454+georgiaallen@users.noreply.github.com>
2024-01-04 16:26:10 +0000

    Update UFO2.java

--- UFO2.java
+++ UFO2.java
@@ -1,159 +1,137 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class UFO2 here.
+ * UFO2 class represents the UFO in Mission 2.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Georgia) 
+ * 
  */
 public class UFO2 extends Actor
 {
-    /**
-     * Act - do whatever the UFO2 wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-   boolean touchingMeteor = false;
+    // Check if UFO2 is currently colliding with a meteor
+    boolean touchingMeteor = false;
     
+    // Variables for UFO2 movement and gravity
     private int speed = 7;
     private int vSpeed = 0;
     private int gravity = 2;
     
-    // time an astronout has to move away from metor before health reduces again
+    // Time UFO2 has to move away from the meteor before health reduces again
     private final int COLLISION_TIMEOUT = 100;
     public int collisionTimer = 0;
     
+    // Reference to MyWorld2 where UFO2 exists
     private MyWorld2 myworld2;
-  
-    //NOTE: not needed
-    //private healthBar healthBar;
-    
+
+    /**
+     * Constructor for UFO2 class.
+     * The world in which UFO2 exists.
+     */
     public UFO2(MyWorld2 myworld2) {
         this.myworld2 = myworld2;
     }
 
-//NOTE: Don't have two Astronaut() constructors
-//public Astronaut(healthBar healthBar) {
-//        this.healthBar = healthBar;
-//    }
-
+    /**
+     
+     */
     public void act() {
         move();
         fall();
         hitMeteor();
         hitAstronaut2();
-          if (onGround()) {
-          Greenfoot.setWorld(new GameOver2());
-                Greenfoot.stop();
+        
+        // Check if UFO2 has hit the ground
+        if (onGround()) {
+            Greenfoot.setWorld(new GameOver2());
+            Greenfoot.stop();
         }
     }
-//NOTE: not needed, doesn't accomplish anything
-//     public void setMyWorld(MyWorld world) {
-//        myworld = world;  
-//    }
-  
 
+    /**
+     * Check if UFO2 is on the ground.
+     * 
+     */
     public boolean onGround() {
         Actor under = getOneObjectAtOffset(0, getImage().getHeight() / 16, Ground.class);
         return under != null;
-        
-           }
+    }
     
-  public void move()
-    {
-        
-    {
-        if(Greenfoot.isKeyDown("s"))
-        {
+    /**
+     * Move UFO2 based on user input - W, A, S, D
+     */
+    public void move() {
+        if(Greenfoot.isKeyDown("s")) {
             setRotation(90);
             move(1);
             setRotation(0);
         }
         
-        if(Greenfoot.isKeyDown("w"))
-        {
+        if(Greenfoot.isKeyDown("w")) {
             setRotation(270);
             move(3);
             setRotation(0);
         }
         
-        if(Greenfoot.isKeyDown("d"))
-        {
+        if(Greenfoot.isKeyDown("d")) {
             move(2);
         }
         
-        if(Greenfoot.isKeyDown("a"))
-        {
+        if(Greenfoot.isKeyDown("a")) {
             setRotation(180);
             move(2);
             setRotation(0);
         }
     }
-}
 
-    
-
- public void hitMeteor() {
-    //healthBar healthbar2 = myworld.getheHealthBar();
-    //healthbar2.loseHealth();
-    
-    Actor meteor = getOneIntersectingObject(Meteor.class);
-    
-    
-    // astronaut is overlapping with a meteor
-    if (meteor != null) {
-        if (this.collisionTimer == 0) {
-            // decrease health on the world healthbar
-            myworld2.healthbar2.loseHealth();
+    /**
+     * Check for collision with meteors and handle health reduction.
+     */
+    public void hitMeteor() {
+        Actor meteor = getOneIntersectingObject(Meteor.class);
         
-            if (myworld2.healthbar2.getHealth() <= 0) {
+        // Check if UFO2 is overlapping with a meteor
+        if (meteor != null) {
+            if (this.collisionTimer == 0) {
+                // Decrease health on healthbar2
+                myworld2.healthbar2.loseHealth();
                 
-                Greenfoot.setWorld(new GameOver2());
-                Greenfoot.stop();
-                  
+                if (myworld2.healthbar2.getHealth() <= 0) {
+                    // Game over if health reaches zero
+                    Greenfoot.setWorld(new GameOver2());
+                    Greenfoot.stop();
+                }
+                
+                // Reset collision timer
+                this.collisionTimer = COLLISION_TIMEOUT;
+            } else {
+                // Decrease timer by 1
+                this.collisionTimer = this.collisionTimer - 1;
             }
-            // reset timer
-            this.collisionTimer = COLLISION_TIMEOUT;
         } else {
-            // decrease timer by 1
-            this.collisionTimer = this.collisionTimer - 1;
+            this.collisionTimer = COLLISION_TIMEOUT;
         }
-    } else {
-        this.collisionTimer = COLLISION_TIMEOUT;
     }
-      
-    
-    /*
-    if (!touchingMeteor) {
-        if (healthBar != null) {
-            myworld.healthbar.loseHealth();
-        
-            touchingMeteor = true;
-            if (healthbar2.getHealth() <= 0) {
-                System.out.println("Game Over!");
-                Greenfoot.stop();
-            }
+
+    /**
+     * Check for collision with astronaut and trigger a win screen.
+     */
+    public void hitAstronaut2() {
+        Actor astronaut2 = getOneIntersectingObject(Astronaut2.class);
+        if (astronaut2 != null) {
+            // You win if the UFO collides with an astronaut
+            Greenfoot.setWorld(new YouWin2());
+            Greenfoot.stop();
         }
-    } else {
-        touchingMeteor = false;
     }
-    */
+
+    /**
+     * Create gravity effect by making the UFO fall.
+     */
+    public void fall() {
+        setLocation(getX(), getY() + vSpeed);
+        vSpeed = gravity;
+    }
 }
-
-            public void hitAstronaut2(){
-    Actor astronaut2 = getOneIntersectingObject(Astronaut2.class);
-         if (astronaut2 != null) {
-        
-        Greenfoot.setWorld(new YouWin2());
-        Greenfoot.stop();
-     }
-    }
-
-
-public void fall(){
-    
-        setLocation (getX(),getY()+ vSpeed);
-            vSpeed = gravity;
-            
     
     
         }}
