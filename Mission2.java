abaeffc6811c11798ca0b3376195b311a9a0f821
libbyweaver <147418909+libbyweaver@users.noreply.github.com>
2024-01-05 14:55:03 +0000

    Update Mission2.java

--- Mission2.java
+++ Mission2.java
@@ -1,23 +1,34 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Mission 2 notes with both players objectives for the mission
-
+ * Mission2 class represents the second mission in the Greenfoot scenario.
+ * This class extends the World class, which is the base class for world objects in Greenfoot.
+ * It means that instances of the Mission2 class define the world where the second mission takes place.
  * 
- * @author (Georgia) 
+ * Mission 2 notes with both players' objectives for the mission.
  * 
+ * Author: Libby
  */
 public class Mission2 extends World
 {
+    /**
+     * Constructor for objects of class Mission2.
+     */
     public Mission2()
     {    
-        super(3000, 700, 1);//screen size
+        super(3000, 700, 1); // Screen size of the world
     }
     
-    public void act(){
-        if(Greenfoot.isKeyDown("enter")){
-        Greenfoot.setWorld(new MyWorld2());//when player presses enter this will move screen to MyWorld2 so Mission 2 gameplay can commence
-    
+    /**
+     * Act method is called automatically in the Greenfoot environment.
+     */
+    public void act()
+    {
+        // Check if the "enter" key is pressed
+        if(Greenfoot.isKeyDown("enter"))
+        {
+            // When the player presses enter, switch to MyWorld2 for Mission 2 gameplay
+            Greenfoot.setWorld(new MyWorld2());
+        }
     }
-}
 }
