d25ce46cc79801805011abee9725d81277be4fcf
libbyweaver <147418909+libbyweaver@users.noreply.github.com>
2024-01-05 14:48:38 +0000

    Update Rocket.java

--- Rocket.java
+++ Rocket.java
@@ -1,10 +1,11 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Rocket here.
+ * Rocket class represents a rocket in the Greenfoot scenario.
+ * This class extends the Actor class, which is the base class for all actors in Greenfoot.
+ * It means that objects of the Rocket class can be added to the world and will be displayed.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Author: Libby
  */
 public class Rocket extends Actor
 {
@@ -15,5 +16,8 @@ public class Rocket extends Actor
     public void act()
     {
         // Add your action code here.
+        // This is where you define the behavior of the Rocket when it acts in the scenario.
+        // You can add code to make the Rocket move, check for collisions, respond to user input, etc.
+        // This method is automatically called by Greenfoot when the 'Act' or 'Run' button is pressed.
     }
 }
