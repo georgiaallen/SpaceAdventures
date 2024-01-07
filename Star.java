a6b197422b7a9c0196fe3a117f517c0364bd9f1f
georgiaallen <147187454+georgiaallen@users.noreply.github.com>
2024-01-07 17:30:35 +0000

    Update Star.java

--- Star.java
+++ Star.java
@@ -2,37 +2,33 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Star class represents a star in the Greenfoot scenario.
- * This class extends the Actor class, which is the base class for all actors in Greenfoot.
- * It means that objects of the Star class can be added to the world and will be displayed.
+ * When the astronaut collects a star object it increases the score counter by 1 
  * 
  * Author: Libby
  */
 public class Star extends Actor
 {
-    /**
-     * Act - do whatever the Star wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    
     public void act()
     {
         // Check if the right arrow key is pressed
         if(Greenfoot.isKeyDown("right"))
         {
-            // Move the star to the left (negative x direction)
+            // Move the star to the left 
             move(-4);
         }
         
         // Check if the left arrow key is pressed
         if(Greenfoot.isKeyDown("left"))
         {
-            // Move the star to the right (positive x direction)
+            // Move the star to the right 
             move(4);
         }
         
         // Check if the star has reached the left edge of the world
         if (getX() == 0)
         {
-            // Wrap around to the right edge and reposition vertically with a random Y-coordinate
+            // 
             setLocation(getWorld().getWidth() - 1, Greenfoot.getRandomNumber(600) + 100);
         }
     }
