4b56128154c45ff61d7a7d5131c5aa4c4c853f30
Saharsh05 <147410862+Saharsh05@users.noreply.github.com>
2024-01-07 00:54:08 +0000

    Update GameOver2.java

--- GameOver2.java
+++ GameOver2.java
@@ -1,13 +1,15 @@
 import greenfoot.*;  
 /**
  * Game over screen for Astronaut and Astronaut2 actor
- * @author (Georgia) 
+ * @author (Saharsh) 
  *
  */
 public class GameOver2 extends World
 {
+    // Constructor for the GameOver2 class 
     public GameOver2()
     {    
+        // Sets the dimensions of the game over screen when certain objectives are met 
         super(3000, 700, 1); //screen size
     }
 }
