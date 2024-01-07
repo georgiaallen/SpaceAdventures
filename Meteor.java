6cbd4091fd662cc31f78311e4869c1124805ab78
georgiaallen <147187454+georgiaallen@users.noreply.github.com>
2024-01-07 17:16:17 +0000

    Update Meteor.java

--- Meteor.java
+++ Meteor.java
@@ -1,38 +1,38 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Meteor class represents a meteor in the Greenfoot scenario.
- * This class extends the Actor class, which is the base class for all actors in Greenfoot.
- * It means that objects of the Meteor class can be added to the world and will be displayed.
+ * Meteor class represents a meteor object in the world which acts as an obstacle to the playable characters.
+ * Playable characters must avoid meteors in order to retain health.
+ * 
+ * 
  * 
  * Author: Libby 
  */
 public class Meteor extends Actor
 {
-    /**
-     * Act - do whatever the Meteor wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    
     public void act()
     {
+        // Repositions meteors when they reach the edge of the screen to create a scrolling effect on the gameplay as the charcters move in the game.
+        
         // Check if the right arrow key is pressed
         if(Greenfoot.isKeyDown("right"))
         {
-            // Move the meteor to the left (negative x direction)
+            // Move the meteor to the left 
             move(-4);
         }
         
         // Check if the left arrow key is pressed
         if(Greenfoot.isKeyDown("left"))
         {
-            // Move the meteor to the right (positive x direction)
+            // Move the meteor to the right
             move(4);
         }
         
         // Check if the meteor has reached the left edge of the world
         if (getX() == 0)
         {
-            // Wrap around to the right edge and reposition vertically with a random Y-coordinate
+            // Reposition meteor on screen in random formation as it reaches edge of world
             setLocation(getWorld().getWidth() - 1, Greenfoot.getRandomNumber(365) + 100);
         }
     }
