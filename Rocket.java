5a73efe3bfc78864fae2f2d7070111f402363cc4
georgiaallen <147187454+georgiaallen@users.noreply.github.com>
2024-01-07 17:25:00 +0000

    Update Rocket.java

--- Rocket.java
+++ Rocket.java
@@ -2,8 +2,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Rocket class represents a rocket in the Greenfoot scenario.
- * This class extends the Actor class, which is the base class for all actors in Greenfoot.
- * It means that objects of the Rocket class can be added to the world and will be displayed.
+ * This object is used as an indicator for end of Mission 1 and transitions players onto Mission 2
  * 
  * Author: Libby
  */
