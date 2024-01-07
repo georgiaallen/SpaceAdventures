4202c59fb01dacf41406d072a8b9cd2402a5315c
georgiaallen <147187454+georgiaallen@users.noreply.github.com>
2024-01-07 17:07:48 +0000

    Update GameOver2.java

--- GameOver2.java
+++ GameOver2.java
@@ -1,6 +1,6 @@
 import greenfoot.*;  
 /**
- * Game over screen for Astronaut and Astronaut2 actor
+ * Game over screen for UFO and UFO2 actor
  * @author (Saharsh) 
  *
  */
@@ -9,7 +9,7 @@ public class GameOver2 extends World
     // Constructor for the GameOver2 class 
     public GameOver2()
     {    
-        // Sets the dimensions of the game over screen when certain objectives are met 
+        // Sets the dimensions of the game over screen when the UFO player loses the game
         super(3000, 700, 1); //screen size
     }
 }
