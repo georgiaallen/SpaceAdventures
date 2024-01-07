c7517df0ca97e8b19889a9aa49c88946f28f05f6
Saharsh05 <147410862+Saharsh05@users.noreply.github.com>
2024-01-07 01:00:56 +0000

    Update Mission1.java

--- Mission1.java
+++ Mission1.java
@@ -1,21 +1,24 @@
 import greenfoot.*;  
 
 /**
- * Mission 1 notes with both players objectives for the mission
- * 
- * @author (Georgia) 
+ * This is the first mission screen in the game and it shows players their objectives based on the player they have selected
+ * Also allows upon pressing the space bar for players to proceed into mission one gameplay 
+ * @author (Saharsh) 
  * 
  */
 public class Mission1 extends World
 {
+    // Constuctor for mission one 
     public Mission1()
     {    
         super(3000, 700, 1);//screen size
     }
-    
+    // The main method, checks if the space bar is pressed to transition to myWorld
     public void act(){
         if(Greenfoot.isKeyDown("space")){
-        Greenfoot.setWorld(new MyWorld());//when player presses space this will move screen to MyWorld so Mission 1 gameplay can commence
+    //when player presses space this will move screen to MyWorld so Mission 1 gameplay can commence
+        Greenfoot.setWorld(new MyWorld());
+            
     }
     }
 }
